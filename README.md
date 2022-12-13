74b3aabc4decfcb988d92075203a9ac1cdf3b971
satitpongjansawang <37213119+satitpongjansawang@users.noreply.github.com>
2022-12-14 05:34:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-line-liff-ma
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-5tuvw2)
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img :src="profile.pictureUrl" width="60" height="60">
+    {{ profile.displayName }}
+    {{ profile.statusMessage }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import liff from '@line/liff'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      loggedIn: false,
+      profile: {},
+      displayName: "",
+    }
+  },
+  mounted(){
+    liff
+    .init({ liffId : '1656167393-ZjmE5mGE'})
+    .then(() => {
+      if (!liff.isLoggedIn()){
+        liff.login()
+      }else{
+        this.loggedIn = liff.isLoggedIn()
+        this.getProfile()
+      }
+    })
+    .catch((err) => {
+      this.occoredError = 'error:' + err
+    })
+  },
+  methods:{
+    getProfile(){
+      liff.getProfile()
+      .then(profile => {
+        this.profile = profile
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
