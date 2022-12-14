0b66e3a688d25a4edb316240460aaeda395c649b
satitpongjansawang <37213119+satitpongjansawang@users.noreply.github.com>
2022-12-14 10:52:58 +0700

    3 files modified

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 const { createApp } = require('vue');
 import App from "./App.vue";
-
+import { BootstrapVue , IconPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 createApp(App).mount("#app");
+
+Vue.use(BootstrapVue)
+Vue.use(IconPlugin)
